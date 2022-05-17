37513741ffe9c144504eb4ef1080741ffb0d6051
pulsie <pulsiedulsie@gmail.com>
2022-05-17 14:24:59 -0700

    game extraction work
    not done yet, but most of the code's written & i just need to add some logic to not download finished rounds & put all the data in a big PGN file for later analysis
    
    thanks to lakin & tors in lichess discord for helping

--- game_extractor.py
+++ game_extractor.py
@@ -2,18 +2,18 @@ import requests
 import ndjson
 import lichessapitoken #this is a local file, you will have to put it locally w a token w study:read access- see README
 apitoken=lichessapitoken.token #only gives study:read access
-print(apitoken)
+#print(apitoken)
 #this gets tournaments which is a good start
-headers={"Authorization": 'Bearer "+apitoken'}
+headers={"Authentication": "Bearer "+apitoken,"Accept": "application/json"}
 answerTournaments=requests.get(url="https://lichess.org/api/broadcast", headers=headers)
 tournaments=answerTournaments.json(cls=ndjson.Decoder)
 for tournament in tournaments:
     for round in tournament["rounds"]:
-        print(round)
+        #print(round)
         id=round["id"]
         
-        rounddata=requests.get(url="https://lichess.org/api/broadcast/-/-/"+id)
-        print(rounddata.url)
-        #print(rounddata.text)
+        rounddata=requests.get(url="https://lichess.org/api/broadcast/round/"+id+".pgn", headers=headers)
+        with open("test.pgn","w") as testfile:
+            testfile.write(rounddata.text)
         break
     break
