d28009144418a889e5e24138fbedc2397b470a87
Jan Zysko <janzysko@gmail.com>
2022-05-19 21:33:50 +0200

    stylistic things

--- game_extractor.py
+++ game_extractor.py
@@ -1,34 +1,35 @@
-import requests 
+import requests
 import ndjson
-import lichessapitoken #this is a local file, you will have to put it locally w a token w study:read access- see README
+import \
+    lichessapitoken  # this is a local file, you will have to put it locally w a token w study:read access- see README
 import time
 import codecs
-apitoken=lichessapitoken.token #only gives study:read access
-#print(apitoken)
-#this gets tournaments which is a good start
-headers={"Authentication": "Bearer "+apitoken,"Accept": "application/json"}
-answerTournaments=requests.get(url="https://lichess.org/api/broadcast", headers=headers, params={"nb":50})
-tournaments=answerTournaments.json(cls=ndjson.Decoder)
+
+api_token = lichessapitoken.token  # only gives study:read access
+# print(api_token)
+# this gets tournaments which is a good start
+headers = {"Authentication": "Bearer " + api_token, "Accept": "application/json"}
+answerTournaments = requests.get(url="https://lichess.org/api/broadcast", headers=headers, params={"nb": 50})
+tournaments = answerTournaments.json(cls=ndjson.Decoder)
 print(len(tournaments))
-x=0
+x = 0
 for tournament in tournaments:
-    x=x+1
-    for round in tournament["rounds"]:
-        #print(round)
-        id=round["id"]
+    x = x + 1
+    for tournament_round in tournament["rounds"]:
+        # print(round)
+        round_id = tournament_round["id"]
         print("requesting")
-        rounddata=requests.get(url="https://lichess.org/api/broadcast/round/"+id+".pgn", headers=headers)
+        round_data_url = f"https://lichess.org/api/broadcast/round/{round_id}.pgn"
+        round_data = requests.get(url=round_data_url, headers=headers)
         print("requested")
-        if(rounddata.status_code==429):
+        if round_data.status_code == 429:
             print("rate limited")
             time.sleep(61)
             print("going again")
-            rounddata=requests.get(url="https://lichess.org/api/broadcast/round/"+id+".pgn", headers=headers)
-        if("%clk" not in rounddata.text):
-            continue;
-        with codecs.open("test.pgn","a", "utf-8") as testfile:
-            testfile.write(rounddata.text)
+            round_data = requests.get(url=round_data_url, headers=headers)
+        if "%clk" not in round_data.text:
+            continue
+        with codecs.open("test.pgn", "a", "utf-8") as testfile:
+            testfile.write(round_data.text)
         print("round done")
     print(f"tournament {x} done")
-    
-
