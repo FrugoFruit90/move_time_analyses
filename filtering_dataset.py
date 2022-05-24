5e6729d754b40faa274d16a09d4509ffb92e2349
Jan Zyśko <janzysko@gmail.com>
2022-05-24 16:28:13 +0200

    allow some rare errors for maybe future dealing with

--- filtering_dataset.py
+++ filtering_dataset.py
@@ -10,21 +10,24 @@ args = parser.parse_args()
 
 game_data = []
 for i, game in enumerate(load_games_from_pgn(args.pgn_file)):
-    if args.n_games is not None and len(game_data) >= args.n_games:
-        break
-    headers = dict(game.headers)
     try:
-        start_time_white = game.next().clock()
-    except AttributeError:
-        continue
-    if start_time_white is None or game.next().clock() < 5000:
-        continue
-    else:
-        clock_values = []
-        moves = []
-        for move in game.mainline():
-            clock_values.append(move.clock())
-        headers['clocks'] = clock_values
-        headers['moves'] = str(game.mainline())
-        game_data.append(headers)
+        if args.n_games is not None and len(game_data) >= args.n_games:
+            break
+        headers = dict(game.headers)
+        try:
+            start_time_white = game.next().clock()
+        except AttributeError:
+            continue
+        if start_time_white is None or game.next().clock() < 5000:
+            continue
+        else:
+            clock_values = []
+            moves = []
+            for move in game.mainline():
+                clock_values.append(move.clock())
+            headers['clocks'] = clock_values
+            headers['moves'] = str(game.mainline())
+            game_data.append(headers)
+    except Exception as exception:
+        print(f"error while processing game {i}, exception: {exception}")
 pd.DataFrame(game_data).to_csv("clock_data.csv", index=False)
