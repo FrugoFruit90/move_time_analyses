8da3111d3e2f80666c9ccf833e60c6100df25963
Jan Zysko <janzysko@gmail.com>
2022-07-16 16:04:23 +0200

    make analyser PEP-compliant

--- game_analyser.py
+++ game_analyser.py
@@ -1,15 +1,18 @@
 import csv
-import re
 import json
-rows  = []
+
+rows = []
 gameClocks = []
 
-def addedTimeDetector(clockList):
-    #takes one player's clocks and determines if there was added time encountered
-    for i in range(0,len(clockList)-1):
-        if (clockList[i+1] - clockList[i]) > 60:
+
+def added_time_detected(clock_list):
+    # takes one player's clocks and determines if there was added time encountered
+    for i in range(0, len(clock_list) - 1):
+        if (clock_list[i + 1] - clock_list[i]) > 60:
             return True
     return False
+
+
 with open("clock_data_small.csv") as csvfile:
     filereader = csv.reader(csvfile)
     for row in filereader:
@@ -17,20 +20,17 @@ with open("clock_data_small.csv") as csvfile:
     rows.pop(0)
 
 for row in rows:
-    #TODO: add checks for time control
+    # TODO: add checks for time control
     try:
         clocks = json.loads(row[18])
     except:
         print(row[3])
-        continue; 
-        #this happens if there's None in the clocks, which probably means that the data went missing somewhere
-    whiteclocks = clocks[::2]
-    blackclocks = clocks[1::2]
-    if not (addedTimeDetector(whiteclocks) or addedTimeDetector(blackclocks)):
-        gameClocks.append([whiteclocks,blackclocks])
-        #this means that time formats where there's added time enountered are removed
-        #note that games in time formats where there's added time, 
-        #but the game didnt last long enough for time to be added are not filtered here!
-    
-    
-    
+        continue
+        # this happens if there's None in the clocks, which probably means that the data went missing somewhere
+    white_clocks = clocks[::2]
+    black_clocks = clocks[1::2]
+    if not (added_time_detected(white_clocks) or added_time_detected(black_clocks)):
+        gameClocks.append([white_clocks, black_clocks])
+        # this means that time formats where there's added time encountered are removed
+        # note that games in time formats where there's added time,
+        # but the game didnt last long enough for time to be added are not filtered here!
